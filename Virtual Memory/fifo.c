b2479bcf046aefc28c08a7228beaffda1e1b57f1
Mírian <contato.mirianfs@gmail.com>
2018-06-10 02:43:22 -0300

    Update fifo.c

--- Virtual Memory/fifo.c
+++ Virtual Memory/fifo.c
@@ -4,31 +4,31 @@
 #include "fifo.h"
 //FIFO (Fist-In-First-Out)
 
-int full_fifo=0;//Verifica se todos os frames est„o preenchidos
-int pagerep_fifo(int p,int qpage){ // page - replaced (repıe p·gina)
-  int temp,repp = 0; // p·gina q derev· ser substituida
+int full_fifo=0;//Verifica se todos os frames est√£o preenchidos
+int pagerep_fifo(int p,int qpage){ // page - replaced (rep√µe p√°gina)
+  int temp,repp = 0; // p√°gina q derev√° ser substituida
 	qpage = 0;
 	int *frame;
 	frame = malloc(qpage*(sizeof(int)));
 
   temp = frame[repp];
   frame[repp] = p;
-  repp++;//O ponteiro se move para o prÛximo frame desde o frame atual se tornou o mais novo
+  repp++;//O ponteiro se move para o pr√≥ximo frame desde o frame atual se tornou o mais novo
   if(repp == qpage)
     repp=0;
-  return temp;   //retorna p·gina q ser· retirada
+  return temp;   //retorna p√°gina q ser√° retirada
   free(frame);
 }
 
 void pagefault_fifo(int p, int qpage){
 	int frame[qpage];
 	if(full_fifo != qpage)
-    frame[full_fifo++] = p;//AtÈ que todos os frames estejam cheios, n„o h· pedido de substituiÁ„o de p·gina
+    frame[full_fifo++] = p;//At√© que todos os frames estejam cheios, n√£o h√° pedido de substitui√ß√£o de p√°gina
   else
 		pagerep_fifo(p,qpage);
 }
 
-int findpage_fifo(int p, int qpage){//Pesquisar e retornar o flag que indica se a p·gina j· est· presente no frame ou n„o
+int findpage_fifo(int p, int qpage){//Pesquisar e retornar o flag que indica se a p√°gina j√° est√° presente no frame ou n√£o
 	int frame[qpage];
 	int i,flag;
   flag=0;
