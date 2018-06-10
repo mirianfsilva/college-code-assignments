1447838f31046874dc94cab9d5f72ff4e06e5d0b
Mírian <contato.mirianfs@gmail.com>
2018-06-10 02:42:32 -0300

    Update fifo.h

--- Virtual Memory/fifo.h
+++ Virtual Memory/fifo.h
@@ -1,8 +1,8 @@
 #ifndef FIFO_H_INCLUDED
 #define FIFO_H_INCLUDED
 
-int pagerep_fifo(int p,int qpage); // p·gina a ser substituÌda
-void pagefault_fifo(int p, int qpage); // Falta de p·gina
+int pagerep_fifo(int p,int qpage); // p√°gina a ser substitu√≠da
+void pagefault_fifo(int p, int qpage); // Falta de p√°gina
 int findpage_fifo(int p, int qpage); // Procura elemento no vetor
 
 
